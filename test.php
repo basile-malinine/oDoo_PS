99a83e8e6bc489d0de25483e4e053311291ea295
basile <basile-m@yandex.ru>
2021-04-30 13:55:16 +0300

    Иммитация экспорта из HZ: Отели (немного доработано)

--- test.php
+++ test.php
@@ -1,198 +1,119 @@
 <?php
   require_once 'vendor/autoload.php';
   require_once 'lib/ripcord-1.0/ripcord.php';
-  // $url = 'http://192.168.100.93:8069';
-  // $db = 'test-odoo-db';
-  // $username = 'basile-m@yandex.ru';
-  // $password = '6grkyxnt';
-  
-  $url = 'http://127.0.0.1:8069';
+
+  $url = 'http://95.165.173.247:8069';
+//  $url = 'http://127.0.0.1:8069';
   $db = 'odoo14';
+//  $db = 'hotels';
   $username = 'roo';
   $password = 'roo';
 
-//  $common = ripcord::client("{$url}/xmlrpc/2/common");
-//  $uid = $common->authenticate($db, $username, $password, []);
-//
-//  print_r($uid);
-//  print_r(PHP_EOL);
+  $arr_exp['hotel'] = [
+          'id' => '1',
+          'created' => '1561369106',
+          'changed' => '1619719545',
+          'name' => 'Отель Вечный Зов',
+          'num_stars' => '812',
+          'address' => 'Народный пр., 5, Москва, 105187',
+          'phone' => '+7985-227-65-09',
+          'email' => 'vechny-zov@mail.ru',
+          'fine_period' => '14',
+          'fine_size' => '1',
+          'arrival_time_std' => '14:00',
+          'departure_time_std' => '05:00',
+          'commission' => '12' ,
+      ];
+  $arr_exp['city'] = [
+          'id' => '847',
+          'name' => 'Москва',
+      ];
+  $arr_exp['hotelier'] = [
+          'id' => '17',
+          'hotel_id' => '1',
+          'created' => '1561368703',
+          'access' => '1619719119',
+          'name' => 'Heorhi Lazarevich',
+          'city' => 'Минск',
+          'address' => 'ул. Натуралистов д.4 кв. 12',
+          'phone' => '296846163',
+          'email' => 'smtpdrupal8+ownerzov@gmail.com',
+      ];
+  $arr_exp['room_types'] = [
+          0 => [
+              'id' => '5',
+              'hotel_id' => '1',
+              'created' => '1561369106',
+              'changed' => '1619719119',
+              'name' => 'Двухместный номер эконом-класса с 1 кроватью или 2 отдельными кроватями',
+              'currency_code' => 'RUB',
+              'price' => '290000',
+              'quantity' => '',
+          ],
+          1 => [
+              'id' => '6',
+              'hotel_id' => '1',
+              'created' => '1561475902',
+              'changed' => '1619718583',
+              'name' => 'Небольшой одноместный номер',
+              'currency_code' => 'RUB',
+              'price' => '120000',
+              'quantity' => '',
+          ],
+          2 => [
+              'id' => '7',
+              'hotel_id' => '1',
+              'created' => '1561633012',
+              'changed' => '1564398067',
+              'name' => ' Стандартный двухместный номер с 1 кроватью или 2 отдельными кроватями',
+              'currency_code' => 'RUB',
+              'price' => ' 320000',
+              'quantity' => '',
+          ],
+          3 => [
+              'id' => '8',
+              'hotel_id' => '1',
+              'created' => '1561633870',
+              'changed' => '1619719545',
+              'name' => 'Полулюкс',
+              'currency_code' => 'RUB',
+              'price' => '460000',
+              'quantity' => '',
+          ],
+          4 => [
+              'id' => '9',
+              'hotel_id' => '1',
+              'created' => '1561633870',
+              'changed' => '1564398518',
+              'name' => 'Семейный полулюкс',
+              'currency_code' => 'RUB',
+              'price' => '490000',
+              'quantity' => '',
+          ],
+          5 => [
+              'id' => '10',
+              'hotel_id' => '1',
+              'created' => '1561633870',
+              'changed' => '1619719545',
+              'name' => 'Люкс',
+              'currency_code' => 'RUB',
+              'price' => '650000',
+              'quantity' => '',
+          ],
+          6 => [
+              'id' => '11',
+              'hotel_id' => '1',
+              'created' => '1561633870',
+              'changed' => '1619718583',
+              'name' => 'Трехместный номер "Комфорт"',
+              'currency_code' => 'RUB',
+              'price' => '310000',
+              'quantity' => '',
+          ],
+      ];
 
-  $uid = 2; // ???
+  $common = ripcord::client("{$url}/xmlrpc/2/common");
+  $uid = $common->authenticate($db, $username, $password, ['interactive' => true]);
   $models = ripcord::client("{$url}/xmlrpc/2/object");
-
-//  print_r($models);
-
-// Вывести все контакты
-function echoHotels()
-{
-    global $models, $db, $uid, $password;
-    $ids = $models->execute_kw($db, $uid, $password,
-        'hotels.hotel', 'search',
-        [[['id', '!=', 0]]]);
-
-    $result = $models->execute_kw($db, $uid, $password,
-        'hotels.hotel', 'read',
-        [$ids],
-        ['fields' => ['id', 'name', 'num_stars',
-            'city_id', 'contracts_ids', 'phone', 'email',
-            'commission', 'invoices_ids', 'contacts_ids']]);
-
-    print_r($result);
-    print_r(PHP_EOL);
-}
-
-function echoContacts()
-{
-    global $models, $db, $uid, $password;
-    $ids = $models->execute_kw($db, $uid, $password,
-        'res.partner', 'search',
-        [[['id', '!=', 0]]]);
-
-    $result = $models->execute_kw($db, $uid, $password,
-        'res.partner', 'read',
-        [$ids],
-        ['fields' => ['id', 'name', 'phone', 'email', 'is_hotelier']]);
-
-    print_r($result);
-    print_r(PHP_EOL);
-}
-
-//echoContacts();
-//echoHotels();
-//exit();
-
-function testHotel() {
-    global $models, $db, $uid, $password;
-    $models->execute_kw($db, $uid, $password,
-        'hotels.hotel', 'import_test', [[], ['a' => 'Hello World!']]);
-}
-
-/**
-   * Создать контакт
-   * @param $fieldsSet array поля и их значение [fieldname => value, fieldname => value, ...]
-   * @return mixed
-   */
-function createContact(array $fieldsSet) {
-    global $models, $db, $uid, $password;
-    return $models->execute_kw($db, $uid, $password,
-        'res.partner', 'create', [$fieldsSet]);
-}
-
-function createHotel(array $fieldsSet) {
-    global $models, $db, $uid, $password;
-    return $models->execute_kw($db, $uid, $password,
-        'hotels.hotel', 'create', [$fieldsSet]);
-}
-
-function updateHotel(array $ids, array $fieldsSet)
-{
-    global $models, $db, $uid, $password;
-    $models->execute_kw($db, $uid, $password, 'hotels.hotel', 'write',
-        [$ids, $fieldsSet]);
-}
-
-function createContract(array $fieldsSet) {
-    global $models, $db, $uid, $password;
-    return $models->execute_kw($db, $uid, $password,
-        'hotels.contract', 'create', [$fieldsSet]);
-}
-
-
-
-
-$faker = Faker\Factory::create();
-
-// Add Contact
-$fieldsSet = [
-    'name' => $faker->name,
-    'city' => $faker->city,
-    'email' => $faker->email,
-];
-//$contact_id = createContact($fieldsSet);
-
-// Add Hotel
-$fieldsSet = [
-//    'name' => 'Отель 0001',
-//    'num_stars' => 'd',
-//    'phone' => $faker->phoneNumber,
-//    'email' => $faker->email,
-    'contacts_ids' => [
-        0 => 13,
-        1 => 14,
-        2 => 15,
-        3 => 16,
-        4 => 18,
-    ],
-    'hotelier_id' => 17,
-    'city_id' => 2,
-];
-//$hotel_id = createHotel($fieldsSet);
-//print_r($hotel_id);
-updateHotel([3], $fieldsSet);
-
-
-exit();
-
-
-
-
-
-
-
-
-/**
-   * Обновить поля записи контактов
-   * @param $ids array идентификаторы контактов, для обновления
-   * @param $fieldsSet array поля и их значение [fieldname => value, fieldname => value, ...]
-   */
-  function updateContacts(array $ids, array $fieldsSet)
-  {
-    global $models, $db, $uid, $password;
-    $models->execute_kw($db, $uid, $password, 'res.partner', 'write',
-        [$ids, $fieldsSet]);
-  }
-
-  /**
-   * @param $ids array
-   */
-  // function deleteContacts(array $ids) {
-  //   global $models, $db, $uid, $password;
-  //   $models->execute_kw($db, $uid, $password,
-  //       'res.partner', 'unlink',
-  //       array($ids));
-  // }
-
-
-
-
-  // $faker = Faker\Factory::create();
-  // for ($i = 0; $i < 10; $i++) {
-  //   $fieldsSet = [
-  //       'name' => $faker->name,
-  //       'city' => $faker->city,
-  //       'email' => $faker->email,
-  //   ];
-  //   createContact($fieldsSet);
-  // }
-  // echoPartners();
-
-//  createContact($fieldsSet);
-
-
-//  updateContacts([10], $fieldsSet);
-//  deleteContacts([10]);
-
-
-//  //  Вывод всех полей модели res.partner
-//  $res = $models->execute_kw($db, $uid, $password,
-//      'res.partner', 'fields_get',
-//      [],
-////      ['attributes' => [
-////          'string',
-////          'help',
-////          'type'
-////      ]]
-//  );
-//  foreach ($res as $key => $value) {
-//      print_r($key . PHP_EOL);
-//  }
+  $result = $models->execute_kw($db, $uid, $password, 'hotels.import_hz',
+      'import_hz', [[], $arr_exp]);
